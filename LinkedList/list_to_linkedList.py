a07aee29b8933b17fef79c7c1c4b7f126126fc09
Ameer Hamza <61383954+Hamza1-coder@users.noreply.github.com>
2024-05-07 23:41:16 +0500

    Double the linked list value
    Daily Challenge Medium Problem to double the value of Linked List

--- LinkedList/list_to_linkedList.py
+++ LinkedList/list_to_linkedList.py
@@ -1,4 +1,7 @@
 from typing import Optional
+import sys
+sys.set_int_max_str_digits(86000)
+
 class ListNode:
     def __init__(self, val, next=None):
         self.val = val
@@ -24,6 +27,51 @@ def search_element(head: Optional[ListNode], value: int):
         head = head.next
     return 0
 
-LL = list_to_linkedlist([1,2,3,4,5])
-traverse_linkedlist(LL)
-print(search_element(LL, 5))
+def removeNodes(head: Optional[ListNode]) -> Optional[ListNode]:
+    # reverse the Linkded List
+    prev = None
+    cur = head
+    while (cur != None):
+        nextNode = cur.next
+        cur.next = prev
+        prev = cur
+        cur = nextNode
+    
+    # create ans List
+    newHead = prev.val
+    maxVal = newHead.val
+    cur = prev.next
+
+    while(cur!=None):
+        if cur.val >= maxVal:
+            node = cur.val
+            node.next = newHead
+            newHead = node
+            maxVal = node.val
+        cur = cur.next
+
+    return newHead
+
+def doubleIt(head: Optional[ListNode]) -> Optional[ListNode]:
+    a = ""
+    temp = head
+    while head:
+        a+=str(head.val)
+        head = head.next
+    a = str(int(a) * 2)
+    temp1 = temp
+    for i in a:
+        temp.next = ListNode(int(i))
+        temp = temp.next
+    return temp1.next
+
+    
+
+
+
+LL = list_to_linkedlist([9,9,9])
+# traverse_linkedlist(LL)
+DL = doubleIt(LL)
+traverse_linkedlist(DL)
+
+# print(search_element(LL, 5))
